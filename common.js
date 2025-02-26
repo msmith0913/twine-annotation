bfa4e0aba5162ff3a7078bcdf1bc9ef4a5cd3894
Meg Smith <79722923+msmith0913@users.noreply.github.com>
2025-02-26 16:16:52 -0600

    Update common.js

--- common.js
+++ common.js
@@ -1,49 +1,24 @@
 // Defines all the data needed for a step
 
-export class ScrollyData {
-  constructor(storyData, stepData) {
-    this.storyData = storyData;
-    this.stepData = stepData;
+export class editionData {
+  constructor(twineData, lineData) {
+    this.twineData = twineData;
+    this.lineData = lineData;
   }
 }
 
-export class StoryData {
+export class twineData {
   constructor(
-    scrollType,
     title,
-    subtitle,
-    endText,
-    textHorizontalPercentage,
-    footer
+    author,
+    editor,
+    license
   ) {
-    this.scrollType = DOMPurify.sanitize(scrollType);
     this.title = DOMPurify.sanitize(title);
-    this.subtitle = DOMPurify.sanitize(subtitle);
-    this.endText = DOMPurify.sanitize(endText);
-    this.textHorizontalPercentage = DOMPurify.sanitize(
-      textHorizontalPercentage
-    );
-    this.textHorizontalPercentage = stripPercentageCharIfExists(
-      this.textHorizontalPercentage
-    );
-    this.footer = DOMPurify.sanitize(footer);
+    this.author = DOMPurify.sanitize(author);
+    this.editor = DOMPurify.sanitize(editor);
+    this.license = DOMPurify.sanitize(license);
   }
-
-  validate(actionTextIfError) {
-    if (
-      doesValueExist(this.textHorizontalPercentage) &&
-      (!isNumber(this.textHorizontalPercentage) ||
-        (this.textHorizontalPercentage > 99 &&
-          this.textHorizontalPercentage < 1))
-    ) {
-      throw new ScrollyError(
-        actionTextIfError,
-        `Invalid TextHorizontalPercentage value "${this.textHorizontalPercentage}"`,
-        `This determines the percentage of the horizontal space the text will take up, and if specified, must be a number between 1 and 99`
-      );
-    }
-  }
-}
 
 function isNumber(value) {
   return !isNaN(value);
@@ -57,77 +32,34 @@ function stripPercentageCharIfExists(str) {
   return str.endsWith("%") ? str.slice(0, -1) : str;
 }
 
-export class StepData {
+export class lineData {
   constructor(
-    contentType,
-    filePath,
-    altText,
-    latitude,
-    longitude,
-    zoomLevel,
-    text
+    lineID,
+    lineType,
+    line,
+    annotation
   ) {
-    this.contentType = DOMPurify.sanitize(contentType);
-    this.filePath = DOMPurify.sanitize(filePath);
-    this.altText = DOMPurify.sanitize(altText);
-    this.latitude = DOMPurify.sanitize(latitude);
-    this.longitude = DOMPurify.sanitize(longitude);
-    this.zoomLevel = DOMPurify.sanitize(zoomLevel);
-    this.text = DOMPurify.sanitize(text);
+    this.lineID = DOMPurify.sanitize(lineID);
+    this.lineType = DOMPurify.sanitize(lineType);
+    this.line = DOMPurify.sanitize(line);
+    this.annotation = DOMPurify.sanitize(annotation);
   }
 
   validate(actionTextIfError) {
-    const validContentTypes = ["image", "map", "video"];
-    if (!validContentTypes.includes(this.contentType)) {
+    const validLineTypes = ["annotated", "unannotated"];
+    if (!validLineTypes.includes(this.lineType)) {
       throw new ScrollyError(
         actionTextIfError,
-        `Invalid contentType: "${this.contentType}"`,
-        `Valid contentType values are: ${validContentTypes.join(", ")}`
+        `Invalid contentType: "${this.lineType}"`,
+        `Valid contentType values are: ${validLineTypes.join(", ")}`
       );
     }
 
-    if (!this.altText || this.altText.length === 0) {
-      throw new ScrollyError(
-        actionTextIfError,
-        `AltText is a required field`,
-        `Accessiblity requires AltText to be set for all content`
-      );
-    }
-
-    if (
-      this.contentType === "map" &&
-      (this.latitude < -90.0 || this.latitude > 90.0)
-    ) {
-      throw new ScrollyError(
-        actionTextIfError,
-        'Latitude must be between -90.0 and 90.0 for content type "map"'
-      );
-    }
-    if (
-      this.contentType === "map" &&
-      (this.longitude < -180.0 || this.longitude > 180.0)
-    ) {
-      throw new ScrollyError(
-        actionTextIfError,
-        'Longitude must be between -180.0 and 180.0 for content type "map"'
-      );
-    }
-
-    if (this.zoomLevel != null && isNaN(Number(this.zoomLevel))) {
-      throw new ScrollyError(
-        actionTextIfError,
-        `ZoomLevel of ${this.zoomLevel} is invalid`,
-        "ZoomLevel must be a number"
-      );
-    }
-  }
-}
-
-export function validateStepDataArray(stepDataArray, actionTextIfError) {
-  var step = 1;
-  stepDataArray.forEach((stepData) => {
-    stepData.validate(actionTextIfError + ", step " + step);
-    step++;
+export function validateLineDataArray(lineDataArray, actionTextIfError) {
+  var line = 1;
+  lineDataArray.forEach((lineData) => {
+    lineData.validate(actionTextIfError + ", line " + line);
+    line++;
   });
 }
 
